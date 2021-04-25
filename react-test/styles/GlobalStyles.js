cd9e2e71122cb87a2be790477d241eb1c7508385
Jorge Ossa <jorgehossa@gmail.com>
2021-04-25 02:27:55 -0500

    📦 NEW: Add theme and globalStyles

---
+++ react-test/styles/GlobalStyles.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: ${({theme}) => theme.fonts.primary};
+    font-size: ${({theme}) => theme.fonts.sizes.base}px;
+  }
+
+  h1,
+  h2,
+  h3 {
+    margin: 0;
+  }
+
+  h1 {
+    font-size: ${({theme}) => theme.fonts.sizes.h1}px;
+  }
+  h2 {
+    font-size: ${({theme}) => theme.fonts.sizes.h2}px;
+  }
+  h3 {
+    font-size: ${({theme}) => theme.fonts.sizes.h3}px;
+  }
+`
---
+++ react-test/styles/theme.js
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    primary: '#111111',
+    secondary: '#EC1D24',
+    white: '#FFF',
+    gray: '#BEBEBE'
+  },
+  fonts: {
+    primary: '"Roboto Condensed", sans-serif;',
+    sizes: {
+      base: 16,
+      h1: 48,
+      h2: 32,
+      h3: 24,
+      small: 14,
+    },
+    weights: {
+      light: 300,
+      regular: 400,
+      bold: 700,
+    }
+  }
+}
